268ee2cb6acc97a7c624c327eb1f6f431c6fad77
KenanAkbarly <kenanekber735@gmail.com>
2024-05-06 21:48:46 +0400

    first commit

---
+++ lab1/script.js
@@ -0,0 +1,69 @@
+// const arr = [1,2,3,4,7,9,12,45];
+// const newArr = [];
+// for (let i =0;i<arr.length;i++){
+//     if(arr[i] % 2 ===0){
+//         newArr.push(arr[i]*3)
+//     }
+// }
+// console.log(newArr)
+
+// const number = 16;
+// if(number % 2 ===0 && number % 4 ===0 && number % 5 !==0){
+//     console.log("hello")
+// }else{
+//     console.log('bye')
+// }
+
+// const product = 'ccar';
+// switch(product){
+//     case 'Telefon':
+//         console.log('Qiymet:1000azn');
+//         break;
+//     case 'Komputer':
+//         console.log('Qiymet:800azn');
+//         break;    
+//     case 'TV':
+//         console.log('Qiymet:2000azn');
+//         break;
+//     default:
+//         console.log('Mehsul movcud deyil');
+// }
+
+// let number = 16;
+// let newNum = Math.sqrt(number,2)
+// console.log(newNum)
+
+let n = 10;
+let a = 0;
+let b = 1;
+let sum;
+console.log(a)
+console.log(b)
+for (let i = 2;i<n;i++){
+    sum = a + b;// 1 2 3 5 8
+    a=b;// 1 1 2 3 
+    b = sum;//1 2 3 5
+    console.log(sum)
+}
+
+// var a = 'Kenan"s';
+// var a = 'hello ';
+// console.log(a)
+
+
+// let text = 'Salam bu test metnidir';
+// let reverseText = text.split(' ').reverse().join(' ');
+// console.log(reverseText)
+
+// let age = 80;
+
+// if(age<18){
+//     console.log('Suruculuk vesiqesi ala bilmez')
+// }else if(age>=18 && age<=65){
+//     console.log('Suruculuk vesiqesi ala biler')
+// }else{
+//     console.log('Suruculuk vesiqesi ala bilmez')
+// }
+// let a = 'salam'
+// console.table(a)
+ 
